3e7ca2d359602ccedb77a92337f0503d89e0c085
Poorna Maheez Uppala <poornamaheez2000@gmail.com>
2025-12-17 19:07:16 +0530

    edited

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,24 @@
 import type { NextConfig } from "next";
-import path from "node:path";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
+  basePath: isProd ? "/indu-infra-o1" : "",
+
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-    ],
+    unoptimized: true,
   },
-  outputFileTracingRoot: path.resolve(__dirname, "../../"),
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
